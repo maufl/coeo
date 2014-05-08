601356fbc2291c5c48b6fe13eb09daeb05e104fa
Felix Maurer <Felix.Maurer@gmx.net>
2014-05-08 17:29:11 +0200

    Add possibility of registration

--- js/es6/vm/client.js
+++ js/es6/vm/client.js
@@ -5,6 +5,9 @@ export class ClientVM extends Client {
     super()
     this.user = ''
     this.password = ''
+    this.passwordConfirmation = ''
+    this.register = false
+    this.insecure = false
     this.currentTree = ''
     this.buddies = []
 
@@ -17,20 +20,36 @@ export class ClientVM extends Client {
     }
     var [userName, domainName] = this.user.split('@');
     this.host = domainName;
-    console.log(this)
     this.openConnection().then(() => {
       var req = this.connection.sendConnect({}, {version: '0.1'})
       return req.promise
     }).then((response) => {
-      console.log(response.short())
       return this.connection.sendAuthenticate({}, {name: userName, password: this.password}).promise
     }).then((response) => {
-      console.log(response.short())
       this.currentTree = this.user
-      console.log('Loged in!')
     }).catch((err) => {
       console.log("Login failed")
       console.error(err)
+    })
+  }
+
+  signup() {
+    if (this.user == '' || this.password == '' || this.password != this.passwordConfirmation) {
+      return;
+    }
+    var [userName, domainName] = this.user.split('@');
+    this.host = domainName;
+    this.openConnection().then(() => {
+      return this.connection.sendConnect({}, {version: '0.1'}).promise
+    }).then(() => {
+      return this.connection.sendRegister({}, {name: userName, password: this.password}).promise
+    }).then(() => {
+      return this.connection.sendAuthenticate({}, {name: userName, password: this.password}).promise
+    }).then(() => {
+      this.currentTree = this.user
+    }).catch((err) => {
+      console.log("Sign up failed")
+      console.log(err)
     })
   }
 
